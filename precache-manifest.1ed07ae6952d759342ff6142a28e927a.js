3f2b0ee4b707d321d1666eaab049a59c692dbd7b
Luiz Otávio <luizomf@gmail.com>
2020-01-04 00:07:50 -0300

    Updates

---
+++ precache-manifest.1ed07ae6952d759342ff6142a28e927a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c26c9a505679a25ee4ef1f9fda02d80",
+    "url": "/jogo-da-velha-usereducer/index.html"
+  },
+  {
+    "revision": "35aeb02f594805a36a55",
+    "url": "/jogo-da-velha-usereducer/static/css/main.5bff0e4e.chunk.css"
+  },
+  {
+    "revision": "8be4bc65deeafe47306c",
+    "url": "/jogo-da-velha-usereducer/static/js/2.3db478fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/jogo-da-velha-usereducer/static/js/2.3db478fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "35aeb02f594805a36a55",
+    "url": "/jogo-da-velha-usereducer/static/js/main.948de818.chunk.js"
+  },
+  {
+    "revision": "457e3cd107511f35a00c",
+    "url": "/jogo-da-velha-usereducer/static/js/runtime-main.7d80f6c8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/jogo-da-velha-usereducer/precache-manifest.1ed07ae6952d759342ff6142a28e927a.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jogo-da-velha-usereducer/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
